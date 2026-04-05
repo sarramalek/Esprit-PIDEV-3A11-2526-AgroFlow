32ae013b6256bdfdfc1f89c94bfd6835261256ca
MallouliEya <Mallouli.Eya@esprit.tn>
2026-04-05 16:05:03 +0100

    Affichage des mouvements

--- src/Repository/stocks/ArticleRepository.php
+++ src/Repository/stocks/ArticleRepository.php
@@ -15,4 +15,31 @@ class ArticleRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Article::class);
     }
+
+    /**
+     * @return Article[]
+     */
+    public function findBySearchCriteria(?string $search, ?string $categoryId): array
+    {
+        $qb = $this->createQueryBuilder('a');
+
+        // Recherche par nom de produit
+        if ($search) {
+            $qb->andWhere('a.nom LIKE :search')
+                ->setParameter('search', '%' . $search . '%');
+        }
+
+        // Filtrage par catégorie
+        if ($categoryId && $categoryId !== '') {
+            // On fait une jointure avec la catégorie pour être plus performant
+            $qb->join('a.categorie', 'c')
+                ->andWhere('c.id = :catId')
+                ->setParameter('catId', $categoryId);
+        }
+
+        // On peut ajouter un tri par défaut (ex: les plus récents en premier)
+        $qb->orderBy('a.nom', 'ASC');
+
+        return $qb->getQuery()->getResult();
+    }
 }
