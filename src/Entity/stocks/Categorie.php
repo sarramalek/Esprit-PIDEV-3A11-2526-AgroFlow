3292f7d1272741a60eb8224ed184b59b46869b4a
MallouliEya <Mallouli.Eya@esprit.tn>
2026-04-06 23:40:52 +0100

    CRUD Categorie et fonctionnalités

--- src/Entity/stocks/Categorie.php
+++ src/Entity/stocks/Categorie.php
@@ -6,9 +6,12 @@ use App\Repository\stocks\CategorieRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: CategorieRepository::class)]
 #[ORM\Table(name: "categorie")]
+#[UniqueEntity(fields: ['nom'], message: 'Ce nom de catégorie existe déjà.')]
 class Categorie
 {
     #[ORM\Id]
@@ -17,15 +20,26 @@ class Categorie
     private ?int $id = null;
 
     #[ORM\Column(length: 100)]
+    #[Assert\NotBlank(message: "Le nom de la catégorie est obligatoire.")]
+    #[Assert\Length(min: 3, minMessage: "Le nom doit faire au moins 3 caractères.")]
     private ?string $nom = null;
 
-    #[ORM\OneToMany(mappedBy: 'categorie', targetEntity: Article::class)]
+    #[ORM\Column(type: "text", nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: "datetime_immutable")]
+    private ?\DateTimeImmutable $date_creation = null;
+
+    #[ORM\OneToMany(targetEntity: Article::class, mappedBy: "categorie")]
     private Collection $articles;
 
     public function __construct()
     {
         $this->articles = new ArrayCollection();
+        $this->date_creation = new \DateTimeImmutable(); // Date auto à la création
     }
+
+    // --- GETTERS & SETTERS ---
 
     public function getId(): ?int
     {
@@ -42,9 +56,22 @@ class Categorie
         return $this;
     }
 
-    /**
-     * @return Collection<int, Article>
-     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeImmutable
+    {
+        return $this->date_creation;
+    }
+
+    /** @return Collection<int, Article> */
     public function getArticles(): Collection
     {
         return $this->articles;
