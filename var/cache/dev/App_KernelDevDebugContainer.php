cf4591784585bf8c4f7f98c424054944f06cbfad
eya <Eya.Laajimi@esprit.tn>
2026-04-08 21:17:56 +0100

    Update Admin :

--- var/cache/dev/App_KernelDevDebugContainer.php
+++ var/cache/dev/App_KernelDevDebugContainer.php
@@ -16,7 +16,7 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \ContainerIQvf25p\App_KernelDevDebugContainer([
     'container.build_hash' => 'IQvf25p',
-    'container.build_id' => '2f6b5a6e',
-    'container.build_time' => 1775660588,
+    'container.build_id' => 'bc919fb8',
+    'container.build_time' => 1775679204,
     'container.runtime_mode' => \in_array(\PHP_SAPI, ['cli', 'phpdbg', 'embed'], true) ? 'web=0' : 'web=1',
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerIQvf25p');
