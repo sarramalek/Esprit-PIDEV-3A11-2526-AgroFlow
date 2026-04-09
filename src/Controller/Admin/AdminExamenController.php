20c96d53de492d41d7c05c6c88e45c40c3814205
yessminerezgui <131909157+yessminerezgui@users.noreply.github.com>
2026-04-09 09:51:26 +0100

    ajout de dashboard admin(crud ) copie de panne

---
+++ src/Controller/Admin/AdminExamenController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Animals\Examen;
+use App\Form\Animals\ExamenType;
+use App\Repository\Animals\ExamenRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/admin/examens')]
+#[IsGranted('ROLE_ADMIN')]
+final class AdminExamenController extends AbstractController
+{
+    #[Route('', name: 'admin_examens_index', methods: ['GET'])]
+    public function index(Request $request, ExamenRepository $examenRepository): Response
+    {
+        $searchTerm  = $request->query->get('q');
+        $sortBy      = $request->query->get('sort', 'id');
+        $direction   = $request->query->get('direction', 'DESC');
+        $typeFilter  = $request->query->get('type');
+
+        // Admin voit tous les examens (null = pas de filtre user)
+        $examens = $examenRepository->searchExamen($searchTerm, $sortBy, $direction, $typeFilter, null);
+
+        return $this->render('admin/examen/index.html.twig', [
+            'examens'          => $examens,
+            'searchTerm'       => $searchTerm,
+            'currentSort'      => $sortBy,
+            'currentDirection' => $direction,
+            'currentType'      => $typeFilter,
+        ]);
+    }
+
+    #[Route('/new', name: 'admin_examens_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $examen = new Examen();
+        $form   = $this->createForm(ExamenType::class, $examen, ['user' => null]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($examen);
+            $em->flush();
+            $this->addFlash('success', 'Examen ajouté avec succès.');
+            return $this->redirectToRoute('admin_examens_index');
+        }
+
+        return $this->render('admin/examen/form.html.twig', [
+            'form'   => $form,
+            'title'  => 'Ajouter un examen',
+            'examen' => $examen,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'admin_examens_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Examen $examen, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(ExamenType::class, $examen, ['user' => null]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'Examen modifié avec succès.');
+            return $this->redirectToRoute('admin_examens_index');
+        }
+
+        return $this->render('admin/examen/form.html.twig', [
+            'form'   => $form,
+            'title'  => 'Modifier l\'examen',
+            'examen' => $examen,
+        ]);
+    }
+
+    #[Route('/{id}/delete', name: 'admin_examens_delete', methods: ['POST'])]
+    public function delete(Request $request, Examen $examen, EntityManagerInterface $em): Response
+    {
+        if ($this->isCsrfTokenValid('delete_examen_' . $examen->getId(), $request->request->get('_token'))) {
+            $em->remove($examen);
+            $em->flush();
+            $this->addFlash('success', 'Examen supprimé avec succès.');
+        }
+
+        return $this->redirectToRoute('admin_examens_index');
+    }
+}
