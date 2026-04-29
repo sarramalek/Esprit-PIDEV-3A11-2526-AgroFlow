17c74dd7ac8cad15239d804698e442bce161dbed
eya <Eya.Laajimi@esprit.tn>
2026-04-29 15:23:30 +0100

    Analyse de l'application avec DoctrineDoctor

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    
 ];
