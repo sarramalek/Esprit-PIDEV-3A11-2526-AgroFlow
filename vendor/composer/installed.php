88dea44e992410cea69c44feed849842e0479223
sarra <benmaleksarra62@gmail.com>
2026-05-11 21:53:10 +0100

    pin PHP 8.2 via railpack

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'a35bf6a0ad355462bfcf288ae4c133dd089093b7',
+        'reference' => 'dc7a07fc06069a277a877c1678522855bc93b07a',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'a35bf6a0ad355462bfcf288ae4c133dd089093b7',
+            'reference' => 'dc7a07fc06069a277a877c1678522855bc93b07a',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
