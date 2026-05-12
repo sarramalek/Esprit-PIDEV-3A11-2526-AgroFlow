3f1e756606bd60335322b02044548ce8c2926b9a
sarra <benmaleksarra62@gmail.com>
2026-05-12 23:21:42 +0100

    amelioration

--- importmap.php
+++ importmap.php
@@ -25,4 +25,11 @@ return [
     '@hotwired/turbo' => [
         'version' => '7.3.0',
     ],
+    'leaflet' => [
+        'version' => '1.9.4',
+    ],
+    'leaflet/dist/leaflet.min.css' => [
+        'version' => '1.9.4',
+        'type' => 'css',
+    ],
 ];
